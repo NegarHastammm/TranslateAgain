546d89297b9e8d3349fa4b8aee6837dfa4577e82
mhmdnsr <mhmdnsrpv@gmail.com>
2025-11-02 18:30:14 +0330

    mhmd changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-
+import "./globals.css";
 export default function Home() {
   return (
-    <div>Github Connection Test</div>
+    <div className="flex flex-row gap-4 bg-amber-100">
+    <p>Test</p>
+    <p>Test</p>
+    <p>Test</p>
+    <p>Test</p>
+    </div>
   );
 }
